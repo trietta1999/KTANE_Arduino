bd10d3f4d98be8f68e05e370204d6190d805b37b
Triet Ta <tadongthuyuyen@gmail.com>
2025-06-22 21:33:58 +0700

    Update Service - Add comment code - Delete timer function - Add Hardware.h - Add endless timer to common data - Update window message - Add header, trailer string for Serial
    # Conflicts:
    #	ESP32/src/CommonData.cpp
    #	ESP32/src/CommonData.h
    #	ESP32/src/CommonDataType.h
    #	ESP32/src/CommonService.cpp

--- ESP32/src/CommonData.cpp
+++ ESP32/src/CommonData.cpp
@@ -18,13 +18,12 @@ namespace sys_host {
     CData<uint8_t> BatteryNum;
     CData<uint32_t> RandomSeed;
     CData<std::pair<uint8_t, uint8_t>> TimeClock;
+    CData<std::tuple<uint16_t, uint16_t, uint16_t>> EndlessTimeClock;
     CData<uint8_t> StrikeNum;
     CData<uint16_t> TimeCycle;
     CData<bool> StrikeState;
-    CData<bool> TimeOut;
-#ifdef _WIN64
     CData<JsonDocument> JsonResponse;
-#endif
+    CData<bool> ModuleStatus;
 }
 
 namespace sys_gui
