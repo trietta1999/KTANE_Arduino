9d277dea84ce8f637cc427894064acb183d3a680
nhuthien <vnamta@gmail.com>
2025-06-28 22:01:33 +0700

    Migrate LibEngine for feature_Button

---
+++ Simulator/LvglWindowsDesktopApplication/LibEngine.h
@@ -0,0 +1,11 @@
+﻿/**
+ * @file  LibEngine.h
+ * @brief Library engine header
+ */
+
+#include "..\LvglPlatform\lvgl\src\misc\lv_types.h"
+
+#pragma once
+void ButtonModule(lv_event_t* e);
+bool CheckTimerNumbers(int number);
+void CheckStripColor(lv_event_t* e);
