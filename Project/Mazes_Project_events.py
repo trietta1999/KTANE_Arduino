56f56a0048e1d837ebedddad1e9697e09d461822
Triet Ta <tadongthuyuyen@gmail.com>
2025-07-20 22:19:02 +0700

    Add Maze generator tool

---
+++ Project/Mazes_Project_events.py
@@ -0,0 +1,16 @@
+
+def OnFreAdjustChange(event_struct):
+    return
+
+
+def OnButtonTXClick(event_struct):
+    return
+
+
+def OnBrightnessChange(event_struct):
+    return
+
+
+def OnButtonClick(event_struct):
+    return
+
