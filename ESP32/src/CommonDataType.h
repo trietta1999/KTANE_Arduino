f12499c6e83736979183a492b069bfae75a1fed7
Triet Ta <tadongthuyuyen@gmail.com>
2025-08-03 20:14:40 +0700

    [SRV] Add 2 module name

--- ESP32/src/CommonDataType.h
+++ ESP32/src/CommonDataType.h
@@ -51,9 +51,10 @@ enum
     WM_REQUEST,           // Request/get from other message
     WM_REQUEST_WITH_DATA, // Request/get from other message with data
     WM_RESPONSE,          // Response message from host to client
-    WM_SET_CLIENTSTATE,   // Notify to set ON/OFF status to specific client
+    WM_SET_CLIENTSTATE,   // Notify to set ON/OFF status to all client
     WM_START_ALL,         // Notify for starting all client
     WM_TIMER_GET,         // Client get timer from host
+    WM_TIMER_SET,         // Host set timer
     WM_STRIKENUM_GET,     // Client get strike count from host
     WM_STRIKESTATE_SET,   // Client notify host to set strike status
     WM_SUCCESSSTATE_SET,  // Client notify host to set client success status
@@ -79,9 +80,6 @@ enum
 
 #define STATE_CHECKED 3
 #define STATE_UNCHECK 2
-
-#define COLOR uint32_t
-#define NON_COLOR 0x000001
 
 #define TO_ENUM(enum_name, item) item,
 #define TO_STRING(enum_name, item) { enum_name::item, #item },
@@ -138,6 +136,8 @@ enum
         CREATE(e, VentingGas) \
         CREATE(e, CapacitorDischarge) \
         CREATE(e, Knobs) \
+        CREATE(e, SystemInfo) \
+        CREATE(e, Transporter) \
 
 enum class LABEL_INDICATOR
 {
@@ -219,7 +219,7 @@ enum class BTN_LABEL_TYPE
 EXTERN_MAP_ENUM_STR(COLOR_TYPE)
 EXTERN_MAP_ENUM_STR(BTN_LABEL_TYPE)
 
-extern std::unordered_map<COLOR_TYPE, COLOR> mapColor;
+extern std::unordered_map<COLOR_TYPE, uint32_t> mapColor;
 
 #define SET_CORRECT_EVENT(first, second, num)\
     CorrectEvent.SetValue(std::make_tuple(first, second, num));\
