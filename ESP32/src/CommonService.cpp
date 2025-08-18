68db1c926e7b7f61c271ce653717bec27199980f
Triet Ta <tadongthuyuyen@gmail.com>
2025-08-18 11:45:26 +0700

    Delete unuse message

--- ESP32/src/CommonService.cpp
+++ ESP32/src/CommonService.cpp
@@ -165,12 +165,6 @@ void ProcessRequest(HWND hwnd, uint32_t msg, JsonDocument jsonDocIn)
 
     switch (msg)
     {
-    case WM_START_ALL:
-    {
-        jsonDoc["start"] = (uint8_t)MODULE_STATUS::START;
-    }
-    break;
-
     case WM_TIMER_GET:
     {
         auto time = sys_host::TimeClock.GetValue();
@@ -217,12 +211,6 @@ void ProcessRequest(HWND hwnd, uint32_t msg, JsonDocument jsonDocIn)
     }
     break;
 
-    case WM_STOP_ALL:
-    {
-        jsonDoc["stop"] = true;
-    }
-    break;
-
     default:
         break;
     }
@@ -251,7 +239,6 @@ void ProcessRequest(HWND hwnd, uint32_t msg, JsonDocument jsonDocIn)
     ::SendMessage(hwnd, WM_RESPONSE, NULL, NULL);
 #else
     data_pack_t byteData = { 0 };
-    byteData.target = hwnd;
     byteData.base_msg = WM_RESPONSE;
     byteData.msg = 0;
     strcpy(byteData.data, jsonDocStr);
@@ -296,7 +283,6 @@ JsonDocument CommonSendRequest(uint32_t msg)
     ::SendMessage(hwnd, WM_REQUEST, msg, NULL);
 #else
     data_pack_t byteData = { 0 };
-    byteData.target = (uint8_t)MODULE_NAME::HostTimer;
     byteData.base_msg = WM_REQUEST;
     byteData.msg = msg;
 
@@ -340,7 +326,6 @@ JsonDocument CommonSendRequestWithData(uint32_t msg, JsonDocument jsonValue)
     serializeJson(jsonValue, jsonDocStr);
 
     data_pack_t byteData = { 0 };
-    byteData.target = (uint8_t)MODULE_NAME::HostTimer;
     byteData.base_msg = WM_REQUEST_WITH_DATA;
     byteData.msg = msg;
     strcpy(byteData.data, jsonDocStr);
