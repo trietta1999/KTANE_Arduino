ecad3db5654c9e0e6ee6d13357ce0bdb4f17e3fc
Triet Ta <tadongthuyuyen@gmail.com>
2025-10-04 20:49:52 +0700

    Update ManualBuildSource.bat

--- Tools/ManualBuildSource.bat
+++ Tools/ManualBuildSource.bat
@@ -14,10 +14,11 @@ pause
 start "" feature_Memory\Simulator\LVGL.sln
 start "" feature_MorseCode\Simulator\LVGL.sln
 start "" feature_ComplicatedWires\Simulator\LVGL.sln
+start "" feature_WireSequences\Simulator\LVGL.sln
 start "" feature_Mazes\Simulator\LVGL.sln
-start "" feature_Passwords\Simulator\LVGL.sln
 pause
 
+start "" feature_Passwords\Simulator\LVGL.sln
 start "" feature_needy_VentingGas\Simulator\LVGL.sln
 start "" feature_needy_CapacitorDischarge\Simulator\LVGL.sln
 start "" feature_needy_Knobs\Simulator\LVGL.sln
